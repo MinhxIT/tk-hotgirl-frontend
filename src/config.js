8e72ed25c8683bd4a644c1e2f18836e7d3bb2801
MinhxIT <varanenguyen@gmail.com>
2019-02-15 21:44:24 +0700

    sssss

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
     rootPath:
     process.env.REACT_APP_STATE === 'localhost'
     ?'http://localhost:6969'
-    :'https://tk-hotgirl-server17.herokuapp.com/'
+    :'https://tk-hotgirl-server17.herokuapp.com'
 };
